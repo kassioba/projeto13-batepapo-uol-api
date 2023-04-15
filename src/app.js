88a809dc601f4cc8414eaf4e2ea7819252bde4de
kassioba <kassioluna@gmail.com>
2023-04-15 03:20:15 -0300

    correção do segundo requisito e implementação do terceiro requisito

--- src/app.js
+++ src/app.js
@@ -57,7 +57,9 @@ app.post("/participants", async (req, res) => {
       to: "Todos",
       text: "entra na sala...",
       type: "status",
-      time: `${now.$H}:${now.$m}:${now.$s}`,
+      time: `${now.$H < 10 ? "0" + now.$H : now.$H}:${
+        now.$m < 10 ? "0" + now.$m : now.$m
+      }:${now.$s < 10 ? "0" + now.$s : now.$s}`,
     });
   } catch (err) {
     return res.status(500).send(err.message);
@@ -66,13 +68,54 @@ app.post("/participants", async (req, res) => {
   res.sendStatus(201);
 });
 
-app.get("/paticipants", async (req, res) => {
+app.get("/participants", async (req, res) => {
+  console.log("oi");
+
   try {
     const participants = await db.collection("participants").find().toArray();
 
     res.send(participants);
   } catch (err) {
     return res.status(500).send(err.message);
+  }
+});
+
+app.post("/messages", async (req, res) => {
+  const { to, text, type } = req.body;
+  const { from } = req.headers;
+
+  const messageSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.valid("private_message", "message").required(),
+  });
+
+  const validation = messageSchema.validate(req.body);
+
+  if (validation.error) return res.status(422).send(validation.error);
+
+  try {
+    const resp = await db.collection("participants").findOne({ name: from });
+    if (!resp) return res.sendStatus(422);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+
+  const message = {
+    from,
+    to,
+    text,
+    type,
+    time: `${now.$H < 10 ? "0" + now.$H : now.$H}:${
+      now.$m < 10 ? "0" + now.$m : now.$m
+    }:${now.$s < 10 ? "0" + now.$s : now.$s}`,
+  };
+
+  try {
+    await db.collection("messages").insertOne(message);
+    res.sendStatus(201);
+  } catch (err) {
+    res.status(500).send(err.message);
   }
 });
 
